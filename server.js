bf55d2c3e9dcba2f92a53cb6fa1f5c802224e6c8
tippawan2534 <oundetch.tippawan@gmail.com>
2019-05-02 10:05:14 -0700

    Test deploy to Heroku.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/frontendFramework62'));
 app.get('/*', function (req, res) {
